1931dc2917acc63a2461d8d40db1ddc13b7a716a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-27 15:39:59 -0700

    Added image swapping for head, body, and pants

--- app.js
+++ app.js
@@ -12,38 +12,27 @@ const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
-
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 headDropdown.addEventListener('change', () => {
-    console.log('we are changing something');
-    headEl.style.backgroundImage = `url('')`;
-    // get the value of the head dropdown
-
-    // increment the head change count state
-    
-    // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
+    headEl.style.backgroundImage = `url('../assets/${headDropdown.value}-head.png')`;
+    headCount++;    
 
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
 middleDropdown.addEventListener('change', () => {
-    // get the value of the middle dropdown
-
-    // increment the middle change count state
-    
-    // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url(../assets/${middleDropdown.value}-middle.png)`;
+    middleCount++;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
 bottomDropdown.addEventListener('change', () => {
-    // get the value of the bottom dropdown
-
-    // increment the bottom change count state
-    
-    // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomEl.style.backgroundImage = `url(../assets/${bottomDropdown.value}-pants.png)`;
+    bottomCount++;
     // update the stats to show the new count (call displayStats() to do this work)
 });
 
