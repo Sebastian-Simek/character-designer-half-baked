aa7a1780c54ba352115dc27a76aa03eba4c74ed7
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-27 18:30:27 -0700

    working on creating new html tags

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
-// set state for how many times the user changes the head, middle, and bottom
-// set state for all of the character's catchphrases
 let headCount = 0;
 let middleCount = 0;
 let bottomCount = 0;
 
 let catchPhraseList = [''];
+
 
 headDropdown.addEventListener('change', () => {
     headEl.style.backgroundImage = `url('../assets/${headDropdown.value}-head.png')`;
@@ -41,21 +40,21 @@ bottomDropdown.addEventListener('change', () => {
 catchphraseButton.addEventListener('click', () => {
     catchPhraseList.push(catchphraseInput.value);
     catchphraseInput.value = '';
-    // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
     displayCatchphrases();
 });
 
 function displayStats() {
     reportEl.textContent = `you have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times`;
 }
+// let newTag = document.createElement('div' ['hi']);
 
 function displayCatchphrases() {
-
+    // catchPhraseList = [''];
     for (let catchphraseOut of catchPhraseList) {
         if (catchPhraseList[catchphraseOut]) {
             return catchphraseOut;
-        } catchphrasesEl.append(catchphraseOut);
-    } 
+        } catchphrasesEl.join().append(catchphraseOut);
+    }   
     
         
     // and for each catchphrase
